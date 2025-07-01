7e051c9db7ca87b66d04f06b2d8e2fde5a2fde85
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-07-01 10:43:47 +0200

    Refactor GoldScreen to use StreamBuilder for live gold price updates

--- lib/src/gold/presentation/gold_screen.dart
+++ lib/src/gold/presentation/gold_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:gold_stream_app/src/gold/data/fake_gold_api.dart';
 import 'package:gold_stream_app/src/gold/presentation/widgets/gold_header.dart';
 import 'package:intl/intl.dart';
 
-class GoldScreen extends StatelessWidget {
+class GoldScreen extends StatefulWidget {
   const GoldScreen({super.key});
+
+  @override
+  State<GoldScreen> createState() => _GoldScreenState();
+}
+
+class _GoldScreenState extends State<GoldScreen> {
   @override
   Widget build(BuildContext context) {
     /// Platzhalter für den Goldpreis
@@ -26,11 +33,37 @@ class GoldScreen extends StatelessWidget {
               SizedBox(height: 20),
               // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
               // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
+              StreamBuilder(
+                stream: getGoldPriceStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(
+                      'Ladefehler: ${snapshot.error}',
+                      style: TextStyle(color: Colors.red),
+                    );
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasData) {
+                    goldPrice = snapshot.data!;
+                    return Text(
+                      NumberFormat.simpleCurrency(
+                        locale: 'de_DE',
+                      ).format(goldPrice),
+                      style: Theme.of(context).textTheme.headlineLarge!
+                          .copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                    );
+                  } else {
+                    // If the snapshot has no data, we can return a placeholder
+                    return Text(
+                      'Keine Daten verfügbar',
+                    );
+                  }
+                  // Default fallback widget if none of the above conditions are met
+                  return SizedBox.shrink();
+                },
               ),
             ],
           ),
